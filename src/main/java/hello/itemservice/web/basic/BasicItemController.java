49460353c2f95abcd751de666351f6dcc8232515
injongjeon <jeonij23@gmail.com>
2023-08-03 17:59:38 +0900

    23/08/03

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -37,8 +34,9 @@ public class BasicItemController {
         itemRepository.save(new Item("itemB", 20000, 20));
     }
 
-    @GetMapping("/{iemId}")
+    @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
+        System.out.println("bbbbbb");
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "basic/item";
@@ -50,7 +48,25 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+    public String save2(@ModelAttribute("item") Item item) {
+
+        itemRepository.save(item);
+        return "basic/item";
     }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        System.out.println("aaaaaaaaaaaaaaa");
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "basic/items/{itemId}";
+    }
+
+
 }
